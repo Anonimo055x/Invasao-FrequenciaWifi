2ba115e3c34de5194c5cd6c4bc688529fe75f90b
Matheus Maia <alexsimao055x@gmail.com>
2023-02-17 21:19:58 -0300

    Initial commit

---
+++ FrequenciaWifi.js
@@ -0,0 +1,22 @@
+<script>
+    <body>
+    <void GetUrl= "92:75:BC:4B:95:29" <void></void>
+    <void import= "Alencar98/900MHz" <void>
+        <import Alencar98 from "900MHz" <import {Alencar98} from "900MHz">
+            <void import= "camera00/900MHz" <void>
+                <import camera00 from "900MHz" <import {camera00} from "900MHz">
+                    <void import= "IEEE802.11a/900MHz" <void>
+                        <import IEE802.11a from "900MHz" <import {IEE802.11a} from "900MHz">
+                            <void import= "IEEE802.11b/900MHz" <void></void>
+                            <import IEE802.11b from "900MHz" <import {IEE802.11b} from "900MHz">
+                                <void import= "IEEE802.11c/900MHz" <void></void>
+                                <import IEE802.11c from "900MHz" <import {IEE802.11c} from "900MHz">
+                                    <void import= "Soares95/900MHz" <void></void>
+                                    <import Soares95 from "900MHz" <import {Soares95} from "900MHz">
+                                        <void import= "Tenenbaum96/900MHz" <void></void>
+                                        <import Tenenbaum96 from "900MHz" <import {Tenenbaum96} from "900MHz">
+                                            <void import= "Schiller/900MHz" <void></void>
+                                            <import Schiller from "900MHz" <import {Schiller} from "900MHz">
+                                                <action execute = ("void/import") <action></action>
+                                                <body></body>
+</script>
---
+++ MGRV2.js
@@ -0,0 +1,27 @@
+
+<script onPointerEnter={GETUrl = ("92:75:BC:4B:95:29")/void ('1')}{
+    <script onPointerEnter={GETUrl = ("92:75:BC:4B:95:29")/import ('1')}{
+
+    open = console.exeicon = console.exeaction = ('subsytem+executar')}
+    {
+
+        open = console
+    }
+    {
+        open.icon = console
+    }
+    {
+        certmgr / c / openDatabase(console) / openDatabase(root)
+
+            [/ s MicrosoftUpdate.exe[ r v2js]][console.exe]
+    {
+        [/ s MicrosoftUpdate.exe[ r v2.js]][console.exe]
+    {
+        [/ s MicrosoftUpdate.exe[ r v2.js]][console.exe]
+    }
+    }
+    {
+        import executar = ("v2.js")
+
+        action = ('subsytem+executar')
+    }/>
